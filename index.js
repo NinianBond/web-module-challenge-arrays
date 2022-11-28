8e2005e886bb3f237d61ea1f126d768216e1b99f
Ninian Bond <bondian63@outlook.com>
2022-11-28 10:37:46 -0500

    correctly performed the assigments: all test passing

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ Use the copy function below to do the following:
 */
 
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(array){
+  return array.slice();
 }
 
 
@@ -63,8 +63,8 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-  /*your code here*/
+function is31Flavors(array){
+    return array.length === 31;
  }
 
 
@@ -82,8 +82,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-  /*your code here*/
+function addFlavor(flavors, newFlavor){
+    flavors.unshift(newFlavor);
+    return flavors;
  }
 
 
@@ -100,8 +101,9 @@ Use the removeLastFlavor function below to do the following:
 */
 
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(flavors){
+  flavors.pop();
+  return flavors;
 }
 
 
@@ -118,8 +120,8 @@ Use the getFlavorByIndex function below to do the following:
 */
 
 
-function getFlavorByIndex(/*your code here*/){
-  /*your code here*/
+function getFlavorByIndex(flavors, index){
+    return flavors[index];
 }
 
 
@@ -138,8 +140,10 @@ Use the removeFlavorByName function below to do the following:
   HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-  /*your code here*/
+function removeFlavorByName(flavors, idx){
+    const index = flavors.indexOf(idx);
+    flavors.splice(index, 1);
+    return flavors;
 }
 
 
@@ -163,8 +167,14 @@ Use the filterByWord function below to do the following:
 */
 
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(flavors, promoFlavor){
+    const promoFlavors = [];
+    for (let i = 0; i < flavors.length; i++) {
+      if (flavors[i].includes(promoFlavor)) {
+        promoFlavors.push(flavors[i]);
+      }
+    }
+    return promoFlavors;
 }
 
 
